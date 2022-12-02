a7d34c078d4ab7fc27f72cfbba625271ab832a25
Dreaded_X <tim@huizinga.dev>
2022-12-02 19:35:55 +0100

    Some more cleanup and fixup to get everything started

--- 2022/src/bin/day1.rs
+++ 2022/src/bin/day1.rs
@@ -1,23 +1,32 @@
+// -- Setup & Runners --
 use aoc::Solver;
-
-pub struct Day {}
+pub struct Day;
 fn main() {
     Day::solve();
 }
 #[test]
-fn part1() {
+fn part1_test1() {
     Day::test(aoc::Part::ONE, "test-1", 24000);
 }
 #[test]
-fn part2() {
+fn part2_test1() {
     Day::test(aoc::Part::TWO, "test-1", 45000);
 }
+#[test]
+fn part1_solution() {
+    Day::test(aoc::Part::ONE, "input", 70116);
+}
+#[test]
+fn part2_solution() {
+    Day::test(aoc::Part::TWO, "input", 206582);
+}
 
-
+// -- Solution --
 impl aoc::Solver for Day {
     fn day() -> u8 {
         1
     }
+
     fn part1(input: &str) -> u32 {
         input.split("\n\n")
             .map(|elf| elf.split("\n")
--- 2022/src/bin/template.rs
+++ 2022/src/bin/template.rs
@@ -1,24 +1,25 @@
+// -- Setup & Runners --
 use aoc::Solver;
-
-pub struct Day {}
+pub struct Day;
 fn main() {
     Day::solve();
 }
 #[test]
-fn part1() {
+fn part1_test1() {
     Day::test(aoc::Part::ONE, "test-1", 0);
 }
 
-
+// -- Solution --
 impl aoc::Solver for Day {
     fn day() -> u8 {
         0
     }
+
     fn part1(input: &str) -> u32 {
-        input.len() as u32
+        0
     }
 
     fn part2(input: &str) -> u32 {
-        input.len() as u32
+        0
     }
 }
